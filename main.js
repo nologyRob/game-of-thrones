07dda52ddcfa8b6fc86c19f4ab04363b1506cc3a
nologyRob <rob.hooper@nology.io>
2022-05-31 11:37:30 +0100

    working on checkboxes

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import charactersArr from "./data/data.js";
 const characterContainer = document.querySelector(".character-container")
 const searchInput = document.querySelector("#search")
+const checkboxes = document.querySelectorAll(".nav__checkbox")
+
+const checkedArray = [];
 
 const createCards = (array) => {
   const characterHTML = array.map((character) => {
@@ -9,7 +12,7 @@ const createCards = (array) => {
     <div class="character-card">
       <h1 class="character-card__name">${character.firstName} ${character.lastName}</h1>
       <img class="character-card__image" src=${character.imageUrl} alt="Image of ${character.firstName}">
-      <h2 class="character-card__title>${character.title}</h2>
+      <h2 class="character-card__title">${character.title}</h2>
       <h3 class="character-card__family">${character.family}</h3>
     </div>
     `
@@ -28,11 +31,22 @@ const filterBySearch = (event) => {
  
   const filteredArray = charactersArr.filter((character) => {
 
-    return character.firstName.includes(searchTerm)
+    return character.fullName.toLowerCase().includes(searchTerm.toLowerCase()) || character.title.toLowerCase().includes(searchTerm.toLowerCase()) || character.family.toLowerCase().includes(searchTerm.toLowerCase())
   })
   characterContainer.innerHTML = createCards(filteredArray)
+}
+
+const addToCheckboxArray = (event) => {
+  if(!checkedArray.includes(event.target.id)){
+    checkedArray.push(event.target.id)
+  } else if (checkedArray.includes(event.target.id)){
+    const index = checkedArray.indexOf(event.target.id)
+    checkedArray.splice(index,1)
+  }
+  console.log(checkedArray)
 }
 
 const characterCards = createCards(charactersArr)
 characterContainer.innerHTML = characterCards
 searchInput.addEventListener("input", filterBySearch)
+checkboxes.forEach(checkbox => checkbox.addEventListener("change", addToCheckboxArray))
