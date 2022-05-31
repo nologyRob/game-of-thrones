87198feddd2c6bd23018b125cb2c1d862e6049a0
nologyRob <rob.hooper@nology.io>
2022-05-31 11:00:53 +0100

    game of thrones initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+import charactersArr from "./data/data.js";
+const characterContainer = document.querySelector(".character-container")
+
+const createCards = (array) => {
+  const characterHTML = array.map((character) => {
+    return (
+    `
+    <div class="character-card">
+      <h1 class="character-card__name">${character.firstName} ${character.lastName}</h1>
+      <img class="character-card__image" src=${character.imageUrl} alt="Image of ${character.firstName}">
+      <h2 class="character-card__title>${character.title}</h2>
+      <h3 class="character-card__family">${character.family}</h3>
+    </div>
+    `
+    )
+  }).join("")
+
+  return characterHTML
+}
+
+const characterCards = createCards(charactersArr)
+characterContainer.innerHTML = characterCards
